e051f6df90a1483b927ab40edfa32bb3fba509b3
BrianARuff <thelonewolf198927@gmail.com>
2018-08-28 19:41:25 -0400

    react calculator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <CalculatorDisplay />
+    </div>
+  );
+};
+
+export default App;
